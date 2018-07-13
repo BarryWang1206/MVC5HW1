d3d094ad6be0da68dbcd30d3958a59f1e6fcd7ff
BarryWang1206 <brevilgm@gmail.com>
2018-07-13 13:09:25 +0800

    加入專案檔案。

---
+++ MVC5HW1/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace MVC5HW1
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //將不需要搜尋的WebFormViewEngine移除以加速View頁面被找到的時間
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine()); //只添加RazorViewEngine
+        }
+    }
+}
